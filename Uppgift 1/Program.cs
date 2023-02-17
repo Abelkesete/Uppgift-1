65a9038fb99fd23c052241aed7ae37dbb9c4afb6
Admin <Admin@TEB106019>
2023-02-17 14:58:16 +0100

    Add project files.

---
+++ Uppgift 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" skriv två heltal");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int h = Convert.ToInt32(Console.ReadLine());
+            
+            AREA(b, h);
+           
+        }
+        static void AREA(int b, int h)
+        {
+            Console.WriteLine(b * h);
+        }
+
+
+    }
+}
